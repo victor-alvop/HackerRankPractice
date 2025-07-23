c137e74f4505c74cde35ef448801ebb05cb54a68
victor <vmalvaradop0@gmail.com>
2025-07-22 20:07:30 -0600

    Feat: SwapCase

--- Python/Basic Data Types/SwapCase/SwapCase.py
+++ Python/Basic Data Types/SwapCase/SwapCase.py
@@ -1,17 +1,5 @@
 def swap_case(s):
-    string_list = []
-    for element in s:
-        if element.islower():
-            string_list.append(element.upper())
-        elif element.isupper():
-            string_list.append(element.lower())
-        else:
-            string_list.append(element)
-        
-
-    
-    string_result = "".join(string_list)
-    return string_result
+    return s.swapcase()
 
 
 s = input()
